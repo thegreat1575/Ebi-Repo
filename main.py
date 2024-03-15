5964d97997bf4449545cd5e2e541b33d564a6777
Promise Matawa <thegreat1575@gmail.com>
2024-03-15 15:35:15 +0000

    Added defensive programming to deter unwanted input from the user.

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 def calculation(x, y, operator):
+    try:
 
-    if operator == '+':
-        total = x + y
-        return total
-    elif operator =='-':
-        total = x - y
-        return total
-    elif operator == 'x':
-        total = x * y
-        return total
-    elif operator == '/':
-        total = x / y
-        return total
+        if operator == '+':
+            total = x + y
+            return total
+        elif operator =='-':
+            total = x - y
+            return total
+        elif operator == 'x':
+            total = x * y
+            return total
+        elif operator == '/':
+            total = x / y
+            return total
+    except ZeroDivisionError:
+        print("You cannot divide by zero...")
+
 
 num1 = float(input("Please enter your first number:  ")) 
 num2 = float(input("Please enter your second number: "))  
 
-operator = input("Enter the math symbol you would want to use: ")
+ops = ['+', '-', '/', 'x']
+while True:
+    operator = input("Enter the math symbol you would want to use in your calculation: ")
+    if operator not in ops:
+        print('''Your selected symbol is not valid.
+             Please use one of the following symbols:
+            +(addition), - (subtraction), / (division), x(multiplication)
+          ''')
+        continue
+    else:
+        print('Calculating...')
+        break
 calc = calculation(num1, num2, operator)
 print(calc)
 
